42d7b14ab07f38b17bce5e3070414d831ab10f17
ads112 <85864939+git-coder257@users.noreply.github.com>
2022-09-04 16:11:49 +0100

    new features

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const fs = require("fs")
+const uuid = require("uuid").v4
 
 const app = express()
 app.use(express.json())
@@ -36,7 +37,7 @@ app.get("/presentations", (req, res) => {
 
 app.post("/presentation/:email", (req, res) => {
     try {
-        fs.readFile("./presentations", "utf8", (err, data) => {
+        fs.readFile("./presentations.json", "utf8", (err, data) => {
             if (err){
                 console.error(err)
                 res.json({
@@ -45,18 +46,29 @@ app.post("/presentation/:email", (req, res) => {
                     presentations: []
                 })
             } else {
-                let response = {
-                    success: true,
-                    error: false
-                }
-
                 let file = JSON.parse(data.toString())
                 file.presentationinterests.push(
                     {
                         email: req.params.email,
-                        presentation: req.body.presentation
+                        presentation: req.body.presentation,
+                        uuid: uuid()
                     }
                 )
+
+                fs.writeFile("./presentations.json", JSON.stringify(file, null, 2), "utf8", (err) => {
+                    if (err){
+                        console.error(err)
+                        res.json({
+                            success: false,
+                            error: true
+                        })
+                    } else {
+                        res.json({
+                            success: true,
+                            error: false
+                        })
+                    }
+                })
             }
         })
     } catch (error) {
@@ -64,8 +76,233 @@ app.post("/presentation/:email", (req, res) => {
     }
 })
 
+app.get("/presentationinterests/:passcode", (req, res) => {
+    try {
+        if (req.params.passcode === "8342"){
+            fs.readFile("./presentations.json", "utf8", (err, data) => {
+                if (err){
+                    console.error(err)
+                    res.json({
+                        success: false,
+                        error: true,
+                        presentations: []
+                    })
+                } else {
+                    let response = {
+                        success: true,
+                        error: false,
+                        presentations: []
+                    }
+                    response.presentations = JSON.parse(data).presentationinterests
+    
+                    res.json(
+                        response
+                    )
+                }
+            })
+        } else {
+            res.json({
+                success: true,
+                error: false,
+                presentations: []
+            })
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.put("/presentation/:passcode/:uuid", (req, res) => {
+    try {
+        if (req.params.passcode === "8342"){
+            fs.readFile("./presentations.json", "utf8", (err, data) => {
+                if (err){
+                    console.error(err)
+                    res.json({
+                        success: false,
+                        error: true
+                    })
+                } else {
+                    let response = {
+                        success: true,
+                        error: false
+                    }
+
+                    let file = JSON.parse(data.toString())
+                    let presentations = file.presentationinterests
+                    let index
+                    let presentation
+
+                    presentations.forEach((element, i) => {
+                        if (element.uuid === req.params.uuid){
+                            index = i
+                            presentation = element
+                        }
+                    })
+
+                    if (index === undefined){
+                        console.log(":(")
+                        response.success = false
+                    } else {
+                        presentations.splice(index, 1)
+                        console.log(presentations)
+                        file.presentationinterests = presentations
+                        file.presentations.push(presentation)
+                    }
+
+                    fs.writeFile("./presentations.json", JSON.stringify(file, null, 2), "utf8", (err) => {
+                        if (err){
+                            console.error(err)
+                            res.json({
+                                success: false,
+                                error: true
+                            })
+                        } else {
+                            console.log("I made it here")
+                            res.json(
+                                response
+                            )
+                        }
+                    })
+                }
+            })
+        } else {
+            res.json({
+                success: false,
+                error: false
+            })
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.delete("/presentationinterest/:passcode/:uuid", (req, res) => {
+    try {
+        if (req.params.passcode === "8342"){
+            fs.readFile("./presentations.json", "utf8", (err, data) => {
+                if (err){
+                    console.error(err)
+                    res.json({
+                        success: false,
+                        error: true
+                    })
+                } else {
+                    let response = {
+                        success: true,
+                        error: false
+                    }
+
+                    let file = JSON.parse(data.toString())
+                    let presentations = file.presentationinterests
+                    let index
+
+                    presentations.forEach((element, i) => {
+                        if (element.uuid === req.params.uuid){
+                            index = i
+                        }
+                    })
+
+                    if (index === undefined){
+                        response.success = false
+                    } else {
+                        presentations.splice(index, 1)
+                        console.log(presentations)
+                        file.presentationinterests = presentations
+                    }
+
+                    fs.writeFile("./presentations.json", JSON.stringify(file, null, 2), "utf8", (err) => {
+                        if (err){
+                            console.error(err)
+                            res.json({
+                                success: false,
+                                error: true
+                            })
+                        } else {
+                            res.json(
+                                response
+                            )
+                        }
+                    })
+                }
+            })
+        } else {
+            res.json({
+                success: false,
+                error: false
+            })
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.delete("/presentation/:passcode/:uuid", (req, res) => {
+    try {
+        if (req.params.passcode === "8342"){
+            fs.readFile("./presentations.json", "utf8", (err, data) => {
+                if (err){
+                    console.error(err)
+                    res.json({
+                        success: false,
+                        error: true
+                    })
+                } else {
+                    let response = {
+                        success: true,
+                        error: false
+                    }
+
+                    let file = JSON.parse(data.toString())
+                    let presentations = file.presentations
+                    let index
+
+                    presentations.forEach((element, i) => {
+                        if (element.uuid === req.params.uuid){
+                            index = i
+                        }
+                    })
+
+                    if (index === undefined){
+                        response.success = false
+                    } else {
+                        presentations.splice(index, 1)
+                        console.log(presentations)
+                        file.presentations = presentations
+                    }
+
+                    fs.writeFile("./presentations.json", JSON.stringify(file, null, 2), "utf8", (err) => {
+                        if (err){
+                            console.error(err)
+                            res.json({
+                                success: false,
+                                error: true
+                            })
+                        } else {
+                            res.json(
+                                response
+                            )
+                        }
+                    })
+                }
+            })
+        } else {
+            res.json({
+                success: false,
+                error: false
+            })
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
+
 app.get("/", (req, res) => {
     res.sendFile("./build/index.html")
+})
+
+app.get("/presinterests", (req, res) => {
+    res.sendFile(`${__dirname}/build/index.html`)
 })
 
 app.listen(PORT, () => {
